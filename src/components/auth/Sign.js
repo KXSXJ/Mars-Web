dd07998af2876c22d498119ebe245519702ae080
KXSXJ <dysj12@gmail.com>
2022-09-21 15:56:01 +0900

    Mars

--- src/components/auth/Sign.js
+++ src/components/auth/Sign.js
@@ -19,19 +19,19 @@ const togglePassword = () => {
     <div className="Sign_container">
         <Link to="/"><img id="Mars_logo2" src="./main_logo.png"></img></Link>
         <img id="Sign_background_img"src="./Sign_img/Sign_background.png"></img>
+        <div id="background_top_txt">여러분의 자격시험 능력을 지금 시험해보세요</div>
     <div className="Sign_content_conatiner">
         <Link to="/"><img id="close_button" src="./Sign_img/back_img.png"></img></Link>
         <div className="Sign_in_content">
-            <div id="sign_in_text">Sign in</div>
-
+            <div id="sign_in_text">로그인</div>
         <div id="text_input_content">
-            <div id="input_text">Email or phone number</div>
-            <input type="text" id="idpw" onChange={(e) => setInputId(e.target.value)}  required></input>
+            <div id="input_text">아이디</div>
+            <input type="text" id="idpw" onChange={(e) => setInputId(e.target.value)}  required ></input>
         </div>
 
         <div id="text_input_content">
             <div className="same_line">
-                <div id="input_text">Password</div>
+                <div id="input_text">비밀번호</div>
                 <label on onClick={togglePassword}>
                     {isRevealPassword ? <img id = "input_blind2"src="./Sign_img/eyes.png"></img> : <img id = "input_blind"src="./Sign_img/killeyes.png"></img>}
                     <div id = "input_blind_text">{isRevealPassword ? 'Show' : 'Hide'}</div>
@@ -40,28 +40,13 @@ const togglePassword = () => {
             <input type={isRevealPassword ? 'text' : 'password'} id="idpw" onChange={(e) => setInputId(e.target.value)}  required></input>
         </div>
         
-        <button id="sign_button">Sign in</button>
-        <div className="same_line">
-            <label>     
-            <div className="same_line">
-            <input type="checkbox" id="checkbox_line"/>
-            <div id="text_margin">Remember me</div> 
-            </div>
-            </label>
-            <div id="text_margin2">Need help?</div>
-        </div> 
+        <button id="sign_button">로그인</button>
+   
 
             <div className="bottom_text">
-                <div id="same_line">
-                    <div id="input_text2">Don't have an acount? <a>Sign up</a></div>
-                </div>        
-                <div id="same_line">
-                    <div id="input_text2">This page is protected by Google reCAPTCHA </div>
-                </div> 
-                <div id="same_line">
-                    <div id="input_text2">to ensure you're not a bot. <a>Learn more.</a></div>
-                </div>
+                    <div id="input_text2">계정을 가지고있지 않습니까? <a>회원가입</a></div>    
             </div>
+            
         </div>
         
     </div>
