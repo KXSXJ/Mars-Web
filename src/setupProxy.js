1bbbe35dde26ce83572b88921aca0a23f47d3f36
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2022-10-04 07:53:00 +0900

    Mars

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api',{
-      target: 'http://184.72.49.233:8080/',
+      target: 'http://184.72.49.233:8080',
       changeOrigin: true,
     })
   );
